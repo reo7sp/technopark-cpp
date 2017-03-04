7cb04274f33da9dd7b70d1d6c3915f6cf79367e6
Oleg Morozenkov <reo7sp@users.noreply.github.com>
2017-03-04 14:16:46 +0300

    Works!

--- a.c
+++ a.c
@@ -65,11 +65,19 @@ void write_lines(char **contents) {
 }
 
 
-char** div_format(char **s) {
+void free_str_array(char** arr) {
+    for (char** pLine = arr; *pLine != NULL; ++pLine) {
+        free(*pLine);
+    }
+    free(arr);
+}
+
+
+char** div_format(char** s) {
     const size_t kResultBuffer = 128;
 
     char** result = NULL;
-    size_t resultSize = 0;
+    size_t resultCount = 0;
 
     size_t divLevel = 0;
 
@@ -82,9 +90,17 @@ char** div_format(char **s) {
         bool isInTag = false;
         bool wasSlash = false;
         bool hadAlnumChar = false;
+        bool wasOpenTag = false;
+        bool wasCloseTag = false;
 
-        for (char* pChar = line; *pChar == '\0'; ++pChar) {
+        for (char* pChar = line; *pChar != '\0'; ++pChar) {
             char c = *pChar;
+
+            if (!isInTag && !hadAlnumChar && c != ' ') {
+                hadAlnumChar = true;
+                lineStart = pChar;
+                lineEnd = lineStart;
+            }
 
             switch (c) {
                 case '<':
@@ -108,8 +124,10 @@ char** div_format(char **s) {
                     if (isInTag) {
                         if (wasSlash) {
                             divLevel--;
+                            wasCloseTag = true;
                         } else {
                             divLevel++;
+                            wasOpenTag = true;
                         }
                         isInTag = false;
                     }
@@ -126,25 +144,45 @@ char** div_format(char **s) {
                     break;
             }
 
-            if (!isInTag && !hadAlnumChar && c != ' ') {
-                hadAlnumChar = true;
-                lineStart = pChar;
-                lineEnd = lineStart;
+            if (wasOpenTag) {
+                if (resultCount % kResultBuffer == 0) {
+                    result = realloc(result, sizeof(char*) * (resultCount + kResultBuffer));
+                }
+                char* newLine = malloc(sizeof(char) * (7 + (divLevel - 1) * 4));
+                for (size_t i = 0; i < (divLevel - 1) * 4; ++i) {
+                    newLine[i] = ' ';
+                }
+                memcpy(newLine + (divLevel - 1) * 4, "<div>\n", 7);
+                result[resultCount++] = newLine;
+                wasOpenTag = false;
             }
 
             if (lineStart != lineEnd) {
-                if (resultSize % kResultBuffer == 0) {
-                    resultSize += kResultBuffer;
-                    result = realloc(result, sizeof(char*) * resultSize);
+                if (resultCount % kResultBuffer == 0) {
+                    result = realloc(result, sizeof(char*) * (resultCount + kResultBuffer));
                 }
                 size_t strLen = lineEnd - lineStart;
-                char* newLine = malloc(sizeof(char) * (strLen + divLevel * 4));
+                char* newLine = malloc(sizeof(char) * (strLen + 1 + divLevel * 4));
                 for (size_t i = 0; i < divLevel * 4; ++i) {
                     newLine[i] = ' ';
                 }
+                newLine[strLen + divLevel * 4] = '\n';
                 memcpy(newLine + divLevel * 4, lineStart, sizeof(char) * strLen);
-                result[resultSize++] = newLine;
+                result[resultCount++] = newLine;
                 lineStart = ++lineEnd;
+            }
+
+            if (wasCloseTag) {
+                if (resultCount % kResultBuffer == 0) {
+                    result = realloc(result, sizeof(char*) * (resultCount + kResultBuffer));
+                }
+                char* newLine = malloc(sizeof(char) * (8 + divLevel * 4));
+                for (size_t i = 0; i < divLevel * 4; ++i) {
+                    newLine[i] = ' ';
+                }
+                memcpy(newLine + divLevel * 4, "</div>\n", 8);
+                result[resultCount++] = newLine;
+                wasCloseTag = false;
             }
         }
     }
@@ -152,7 +190,7 @@ char** div_format(char **s) {
     if (result == NULL) {
         result = malloc(sizeof(char*));
     }
-    result[resultSize] = NULL;
+    result[resultCount] = NULL;
 
     return result;
 }
@@ -162,7 +200,7 @@ int main() {
     char** input = read_all_stdin();
     char** output = div_format(input);
     write_lines(output);
-//    free(input);
-//    free(output);
+    free_str_array(input);
+    free_str_array(output);
     return 0;
 }
